df14d4b8bd65f62de9d0be34888f9c7d84adbc9f
Andrii Protas <andreyprotas5@gmail.com>
2023-12-07 15:41:55 +0100

    Add NoteRepository interface

---
+++ src/main/java/com/andriiiiiko/spring/repository/NoteRepository.java
@@ -0,0 +1,9 @@
+package com.andriiiiiko.spring.repository;
+
+import com.andriiiiiko.spring.entity.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+}
